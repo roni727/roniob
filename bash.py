2901dc46bfc45839b4220da86a34368ee5e5d146
roni <hilmiburek7@gmail.com>
2024-11-19 21:34:31 +0800

    Add files via upload

---
+++ bash.py
@@ -0,0 +1,94 @@
+# author : @Syhrularv_
+# -*- coding: utf-8 -*-
+#recode : ARIE TUBE_
+#
+import os
+import sys
+import fileinput
+
+N = '\033[0m'
+D = '\033[90m'
+W = '\033[1;37m'
+B = '\033[1;34m'
+R = '\033[1;31m'
+G = '\033[1;32m'
+Y = '\033[1;33m'
+C = '\033[1;36m'
+
+ask = G + '[' + W + '?' + G + '] '
+sukses = G + '[' + W + '√' + G + '] '
+eror = R + '[' + W + '!' + R + ']'
+
+banner = """
+  ⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄
+⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄
+⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
+⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄
+⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰
+⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗
+⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄
+⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄
+⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄
+⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄
+⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴
+⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿
+ Pandai Menghina Namun Bodoh Untuk Berkaca, Terkadang Manusia Terlalu Kuno Dalam Bermain Logika.
+Umur Hanya Angka, Attitude Diatas Segalanya.
+ ʝαиgαи ℓυρα ѕυвѕ¢яιвє ¢нαииєℓ 🎋RONI ATTCAKER🎋
+""".format(D,W,D,W,D,W,Y,W,D,W,D,W,D,W,D,W,D,Y,D,W,D,Y,D,G,W,G,D,G,W,G,Y,D,Y,D,Y,D,Y,D,Y)
+
+banner2 = """
+   {}[{}1{}]{} Encript      {}[{}2{}]{} Decrypt
+""".format(G,W,G,W,G,W,G,W)
+
+print banner
+print banner2
+
+def dekrip():
+   try:
+       sc = raw_input(ask + W + "Script " + G + "> " + W)
+       f = open(sc,'r')
+       filedata = f.read()
+       f.close()
+
+       newdata = filedata.replace("eval","echo")
+
+       out = raw_input(ask + W + "Output" + G + " > " + W)
+       f = open(out,'w')
+       f.write(newdata)
+       f.close()
+
+       os.system("touch tes.sh")
+       os.system("bash " + out + " > tes.sh")
+       os.remove(out)
+       os.system("mv -f tes.sh " + out)
+       print (sukses + "DONE CIL..")
+
+   except KeyboardInterrupt:
+       print (eror + " Stopped!")
+   except IOError:
+       print (eror + " File Not Found!")
+
+def enkrip():
+   try:
+       script = raw_input(ask + W + "Script " + G + "> " + W)
+       output = raw_input(ask + W + "Output " + G + "> " + W)
+       os.system("bash-obfuscate " + script + " -o " + output )
+       print (sukses + "DONE CIL..")
+   except KeyboardInterrupt:
+       print (eror + " Stopped!")
+   except IOError:
+       print (eror + " File Not Found!")
+
+
+takok = raw_input(W + "Choose" + G + " > ")
+
+if takok == "1" or takok == "01":
+   enkrip()
+elif takok == "2" or takok == "02":
+   dekrip()
+else:
+   print (eror + " Wrong input")
